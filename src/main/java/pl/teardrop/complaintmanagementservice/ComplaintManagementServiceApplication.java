3a3e86290f04753c8f9871347d941c973b66b619
Michał Lewandowski <michal.lewandowski99@gmail.com>
2025-04-13 10:44:20 +0200

    first

---
+++ src/main/java/pl/teardrop/complaintmanagementservice/ComplaintManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.teardrop.complaintmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/teardrop/complaintmanagementservice/ComplaintManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.teardrop.complaintmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
